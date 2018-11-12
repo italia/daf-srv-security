359df41c868a7a985a6fc8637fc34784a4861436
gruggiero <giovanni.ruggiero@gmail.com>
2018-11-12 17:16:26 +0100

    Removed testci kubernetes namespace

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-kubectl --kubeconfig=$KUBECONFIG delete configmap security-manager-conf -n testci || true
-kubectl --kubeconfig=$KUBECONFIG create configmap security-manager-conf -n testci --from-file=../conf/${DEPLOY_ENV}/prodBase.conf
-kubectl --kubeconfig=$KUBECONFIG replace -f daf-security-manager.yml -n testci --force
+kubectl --kubeconfig=$KUBECONFIG delete configmap security-manager-conf || true
+kubectl --kubeconfig=$KUBECONFIG create configmap security-manager-conf --from-file=../conf/${DEPLOY_ENV}/prodBase.conf
+kubectl --kubeconfig=$KUBECONFIG replace -f daf-security-manager.yml --force
